857b90e98a8433f15dbf7650063691bbf52a4047
Wesley Groen <wesleygroen.wg@gmail.com>
2021-05-21 13:49:08 +0200

    script

--- test.sh
+++ test.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
     echo 4 Arguments needed
-    echo ./test.sh program min max output
+    echo ./test.sh program min max timeLimit output
     exit -1
 fi
 
 prog=$1
 let start=$2
 let max=$3
-out=$4
+let t=$4
+out=$5
 
 let i=${start}
 while [ $i -lt ${max} ]; do
     let i+=1
     # echo prun -np $i -16 -script \$PRUN_ETC/prun-openmpi ${prog} \| tee -a ${out} &
-    prun -np $i -16 -t 60 -script \$PRUN_ETC/prun-openmpi ${prog} | tee -a ${out} &
+    prun -np $i -16 -t $t -script \$PRUN_ETC/prun-openmpi ${prog} | tee -a ${out} &
 done
